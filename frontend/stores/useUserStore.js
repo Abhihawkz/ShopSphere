ef73bcb8a50a80c332773cf3811bc79f509279c1
Abhishek <abhipro01@gmail.com>
2025-02-24 22:24:13 +0530

    Added user stores

---
+++ frontend/stores/useUserStore.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import {create} from "zustand";
+import {toast} from "react-hot-toast";
+
+export const useUserStore = create((set,get)=>({
+    user:null,
+    loading:false,
+    checkingAuth:true,
+
+
+    signup:async({name,email,password,confirmPassword}) =>{
+        set({loading:true})
+        if(password != confirmPassword){
+            set({loading:false});
+            return toast.error("Passwords do not match!!!");
+        }
+
+        try {
+            const res = await axios.post("/auth/register",{name,email,password,confirmPassword})
+            set({user:res.data.user,loading:false})
+        } catch (error) {
+            set({loading:false})
+            toast.error(error.response.data.message || "An error occured while signinng up ")
+        }
+    }
+}))
